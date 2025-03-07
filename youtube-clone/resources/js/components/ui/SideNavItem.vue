88f5285ace407a779fffee31465185ab39258b2c
HarpinderSingh1999 <tui57086@temple.edu>
2025-03-07 16:31:12 -0500

    duh

--- youtube-clone/resources/js/components/ui/SideNavItem.vue
+++ youtube-clone/resources/js/components/ui/SideNavItem.vue
@@ -1,6 +1,14 @@
 <template>
-    <li class="text-white text-sm font-semibold text-center hover:bg-gray-700 rounded-lg cursor-pointer">
-        Home
+    <li 
+        :class="[!openSideNav ? 'p-2' : 'flex items-center p-2']"
+        class="text-white text-sm font-semibold text-center hover:bg-gray-700 rounded-lg cursor-pointer">
+        <div :class="[!openSideNav ? 'w-full flex justify-center' : '']">
+            <component :is="icon"/>
+        </div>
+        <div :class="[!openSideNav ? '' : 'mt-1 ml-4']">
+            <span v-if ='openSideNav'>{{ iconString.substring(0,4) }}</span>
+            <span v-else>{{ iconString }}</span>
+        </div>
     </li>
 </template>
 
@@ -22,21 +30,21 @@ const props = defineProps({
 
 const { openSideNav, iconString } = toRefs(props);
 
-let icon = ref(null)
+let icon = null
 
 if (iconString.value === 'Home') {
     icon = Home
 }
 
-if (iconString.value === 'FolderPlayOutline') {
+if (iconString.value === 'Subscriptions') {
     icon = FolderPlayOutline
 }
 
-if (iconString.value === 'ImageMultiple') {
+if (iconString.value === 'Library') {
     icon = ImageMultiple       
 }
 
-if (iconString.value === 'ThumbUpOutline') {
+if (iconString.value === 'Liked') {
     icon = ThumbUpOutline
 }
 
@@ -44,15 +52,15 @@ if (iconString.value === 'History') {
     icon = History
 }   
 
-if (iconString.value === 'ClockOutline') {
+if (iconString.value === 'Watch Later') {
     icon = ClockOutline
 }   
 
-if (iconString.value === 'VideoPlusOutline') {
+if (iconString.value === 'Add Video') {
     icon = VideoPlusOutlineIcon
 }   
 
-if (iconString.value === 'Delete') {
+if (iconString.value === 'Delete Video') {
     icon = Delete      
 }
 </script>
