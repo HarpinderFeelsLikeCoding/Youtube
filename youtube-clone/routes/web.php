3eb1456322794d71ff58938b17edcdf1871f38bd
HarpinderSingh1999 <tui57086@temple.edu>
2025-03-11 12:53:39 -0400

    in delete video

--- youtube-clone/routes/web.php
+++ youtube-clone/routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-return Inertia::render('Welcome');
-})->name('home');
-
+Route::get('/', function () { return Inertia::render('Welcome');})->name('home');
+Route::get('/delete-video', function () { return Inertia::render('DeleteVideo');})->name('deleteVideo');
 Route::get('/videos/{id}', [\App\Http\Controllers\VideosController::class, 'show'])->name('videos.show');
+
+require __DIR__.'/auth.php';
