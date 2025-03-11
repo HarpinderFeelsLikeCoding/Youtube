838e248a70116642c30dbfe8192b4f268df6da7a
HarpinderSingh1999 <tui57086@temple.edu>
2025-03-11 16:18:23 -0400

    overlay part now

--- youtube-clone/resources/js/layouts/NavLayout.vue
+++ youtube-clone/resources/js/layouts/NavLayout.vue
@@ -64,6 +64,7 @@ let openSideNav = ref(true);
         </div>
 
         <div 
+            v-if="$page.url === '/'"
             id='SideNav' 
             class = 'h-[100%] fixed z-0 bg-black'
             :class="[!openSideNav ? 'w-[70px]' : 'w-[240px]']"
@@ -97,8 +98,41 @@ let openSideNav = ref(true);
             </ul>
         </div>
 
-        <div id='SideNavOverlay'>
-            
+        <div>
+            <div 
+            id='SideNavOverlay' 
+            ref="sideNavOverlay"
+            class = 'h-[100%] fixed z-0 bg-black'
+            :class="[!openSideNav ? 'w-[70px]' : 'w-[240px]']"
+        >
+            <ul :class ="[!openSideNav ? 'p-2' : 'px-5 pb-2 pt-[7px]']" class ='mt-[60px] w-full'>
+                <SideNavItem :openSideNav='openSideNav' iconString='Home' />
+                <SideNavItem :openSideNav='openSideNav' iconString='Add Video' />
+                <SideNavItem :openSideNav='openSideNav' iconString='Delete Video' />
+                <div class="border-b border-b-gray-700 my-2.5"></div>
+                <SideNavItem :openSideNav='openSideNav' iconString='Subscriptions' />
+                <SideNavItem :openSideNav='openSideNav' iconString='Library' />
+                <SideNavItem :openSideNav='openSideNav' iconString='Liked' />
+                <SideNavItem :openSideNav='openSideNav' iconString='History' />
+                <SideNavItem :openSideNav='openSideNav' iconString='Watch Later' />
+                <div v-if ='openSideNav'>
+                    <div class="border-b border-b-gray-700 my-2.5"></div>
+                    <div class ='text-gray-400 text-[14px] text-extrabold'>
+                        About Press Copyright
+                        <div>Contact Us</div>
+                        Creator Advertise Developers
+                    </div>
+
+                    <div class="border-b border-b-gray-700 my-2.5"></div>
+
+                    <div class ='text-gray-400 text-[14px] text-extrabold'>
+                        Term Privacy Policy & Safety
+                        <div>How YouTube Works</div>
+                        <span>Test new features</span>
+                    </div>
+                </div>
+            </ul>
+        </div>
         </div>
 
         <div 
@@ -109,6 +143,7 @@ let openSideNav = ref(true);
             }"
         >
             <slot/> 
+
         </div>
     </div>
 </template>
